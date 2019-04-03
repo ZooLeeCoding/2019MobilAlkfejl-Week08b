7e0f6027794e81f12ca7895172dd235bc37c6b55
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2019-04-03 16:35:40 +0200

    sima broadcast mukodik

--- app/src/main/java/hu/szte/mobilalk/maf_02/MainActivity.java
+++ app/src/main/java/hu/szte/mobilalk/maf_02/MainActivity.java
@@ -1,7 +1,10 @@
 package hu.szte.mobilalk.maf_02;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity
     private TextView counterView;
     private TextView helloView;
 
-
+    private BroadcastReceiver br;
 
     public static final String EXTRA_MESSAGE = "hu.szte.mobilalk.maf_02.MESSAGE";
     public static final int TEXT_REQUEST = 1;
@@ -53,6 +56,17 @@ public class MainActivity extends AppCompatActivity
             getSupportLoaderManager().initLoader(0, null,
                     this);
         }
+
+        this.br = new MyReceiver();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        this.registerReceiver(br, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(this.br);
+        super.onDestroy();
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
